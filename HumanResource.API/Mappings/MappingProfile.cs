e321756fd92cb1d94dd8ccf7c95892358cb00f22
JatinSingal <jatinsingal96@gmail.com>
2026-05-08 19:53:46 +0530

    automapper added

---
+++ HumanResource.API/Mappings/MappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using HumanResource.API.DTOs;
+using HumanResource.API.Models;
+
+namespace HumanResource.API.Mappings
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Employee, EmployeeDto>().ReverseMap();
+
+            CreateMap<JobHistory, JobHistoryDto>().ReverseMap();
+        }
+    }
+}
--- HumanResource.API/Program.cs
+++ HumanResource.API/Program.cs
@@ -1,4 +1,6 @@
 
+using HumanResource.API.Mappings;
+
 namespace HumanResource.API
 {
     public class Program
@@ -13,7 +15,7 @@ namespace HumanResource.API
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
-
+            builder.Services.AddAutoMapper(typeof(MappingProfile));
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.

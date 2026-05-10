9007fbb05c5b081ff7394817b0291c44a0815603
JatinSingal <jatinsingal96@gmail.com>
2026-05-10 10:12:24 +0530

    Exception final code

--- HumanResource.API/Middleware/ExceptionMiddleware.cs
+++ HumanResource.API/Middleware/ExceptionMiddleware.cs
@@ -25,11 +25,7 @@ namespace HumanResource.API.Middleware
             }
         }
 
-        private async Task HandleExceptionAsync
-        (
-            HttpContext context,
-            Exception exception
-        )
+        private async Task HandleExceptionAsync(HttpContext context, Exception exception)
         {
             context.Response.ContentType = "application/json";
 

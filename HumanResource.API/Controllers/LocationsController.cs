c85d3d62bb2d2ac564bb6e6542fd11a8b2031b9f
Mehakmiku <jandym1ndy13@gmailcom>
2026-05-11 13:51:47 +0530

    getlocationbycountry error corrected

--- HumanResource.API/Controllers/LocationsController.cs
+++ HumanResource.API/Controllers/LocationsController.cs
@@ -30,6 +30,7 @@ namespace HumanResource.API.Controllers
         [HttpGet("country/{countryId}")]
         public async Task<IActionResult> GetByCountry(string countryId)
         {
+
             return Ok(await _service.GetByCountryAsync(countryId));
         }
 

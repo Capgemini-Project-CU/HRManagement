31ab141ce6a43b19defb49b3143ad0804c9f2d22
Priti <mehtaprity83@gmail.com>
2026-05-13 19:55:25 +0530

    Added Department unit tests and test utilities

---
+++ Test/TestData/DepartmentTestData.cs
@@ -0,0 +1,32 @@
+﻿using HumanResource.API.DTOs;
+using HumanResource.API.Models;
+
+namespace Test.TestData
+{
+    public static class DepartmentTestData
+    {
+        public static DepartmentDto GetDepartmentDto()
+        {
+            return new DepartmentDto
+            {
+                DepartmentId = 10,
+                DepartmentName = "IT",
+                ManagerId = 101,
+                LocationId = 1700,
+                ManagerName = "Steven King",
+                City = "Seattle"
+            };
+        }
+
+        public static Department GetDepartmentEntity()
+        {
+            return new Department
+            {
+                DepartmentId = 10,
+                DepartmentName = "IT",
+                ManagerId = 101,
+                LocationId = 1700
+            };
+        }
+    }
+}

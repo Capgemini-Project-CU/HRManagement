d7ed72e3fb25784db62bec09246a235b08809649
Priti <mehtaprity83@gmail.com>
2026-05-14 12:18:26 +0530

    Updated Department controller tests and test data

--- Test/TestData/DepartmentTestData.cs
+++ Test/TestData/DepartmentTestData.cs
@@ -18,6 +18,43 @@ namespace Test.TestData
             };
         }
 
+        public static DepartmentDto GetInvalidDepartmentDto()
+        {
+            return new DepartmentDto
+            {
+                DepartmentId = 100,
+                DepartmentName = "Invalid",
+                ManagerId = null,
+                LocationId = null
+            };
+        }
+
+        public static List<DepartmentDto> GetDepartmentDtoList()
+        {
+            return new List<DepartmentDto>
+            {
+                new DepartmentDto
+                {
+                    DepartmentId = 10,
+                    DepartmentName = "IT",
+                    ManagerId = 101,
+                    LocationId = 1700,
+                    ManagerName = "Steven King",
+                    City = "Seattle"
+                },
+
+                new DepartmentDto
+                {
+                    DepartmentId = 20,
+                    DepartmentName = "HR",
+                    ManagerId = 102,
+                    LocationId = 1800,
+                    ManagerName = "Neena Kochhar",
+                    City = "New York"
+                }
+            };
+        }
+
         public static Department GetDepartmentEntity()
         {
             return new Department
@@ -26,6 +63,28 @@ namespace Test.TestData
                 DepartmentName = "IT",
                 ManagerId = 101,
                 LocationId = 1700
+            };
+        }
+
+        public static List<Department> GetDepartmentEntityList()
+        {
+            return new List<Department>
+            {
+                new Department
+                {
+                    DepartmentId = 10,
+                    DepartmentName = "IT",
+                    ManagerId = 101,
+                    LocationId = 1700
+                },
+
+                new Department
+                {
+                    DepartmentId = 20,
+                    DepartmentName = "HR",
+                    ManagerId = 102,
+                    LocationId = 1800
+                }
             };
         }
     }
